fc473381465f71adf1fe956521210693053fd78f
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-12 15:55:55 -0600

    Continuing work

--- src/yamada/calculation.py
+++ src/yamada/calculation.py
@@ -100,6 +100,8 @@ def h_poly(abstract_graph):
     """
 
     """
+
+    
     A = pari('A')
     G = abstract_graph
 
@@ -475,6 +477,15 @@ class SpatialGraphDiagram:
 
 
 def get_coefficients_and_exponents(poly):
+
+    """
+    A helper function to extract the coefficients and exponents from a Yamada polynomial.
+
+    The Yamada polynomial calculator was originally written with SageMath and the Laurent polynomial objects
+    had explicit attributes for coefficients and exponents that you could directly query. However, switching
+    to the cypari library to improve OS compatibility added a few complications, including that there is no native
+    method to access the coefficients and exponents of Yamada polynomials. This function obtains them.
+    """
 
     # Assumes all denominators are only A**n with no coefficient
     coefficients = poly.numerator().Vec()
