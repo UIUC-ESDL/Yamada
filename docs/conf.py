1db088a375f47c61a212778d625d30d0a534ad55
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-13 09:52:15 -0600

    Tweak sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,9 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.napoleon']
+
+napoleon_google_docstring = False
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
