9925f53bfebf44df1d9269a30f9353db405cab79
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-25 17:29:46 -0600

    Improve __repr__ method

--- src/yamada/laurent_polynomial.py
+++ src/yamada/laurent_polynomial.py
@@ -77,6 +77,28 @@ class InputValidation:
 
         self.order = order
 
+    def _format_polynomial(self, user_input):
+        """_check_input_format Converts input to LaurentPolynomial for arithmetic operations
+
+        :param user_input: _description_
+        :type user_input: _type_
+        :raises TypeError: _description_
+        :return: _description_
+        :rtype: _type_
+        """
+
+        if isinstance(user_input, LaurentPolynomial):
+            return user_input
+
+        elif isinstance(user_input, int) or isinstance(user_input, float):
+            warn("Converting input to a LaurentPolynomial with a single term")
+            coefficients = [user_input]
+            exponents = [0]
+            return LaurentPolynomial(self.term, coefficients=coefficients, exponents=exponents)
+
+        else:
+            raise TypeError("Polynomial must be of type LaurentPolynomial, int, or float")
+
 
 class LaurentPolynomial(InputValidation):
 
@@ -97,14 +119,10 @@ class LaurentPolynomial(InputValidation):
         param order: The order in which the polynomial is represented (i.e., "increasing" or "decreasing")
         :type order: str
         """
-        # super(LaurentPolynomial, self).__init__(term, coefficients, exponents, order)
+        super(LaurentPolynomial, self).__init__(term, coefficients, exponents, order)
 
-        self._validate_term(term)
-        self._validate_coefficients(coefficients)
+        self._simplify_expression()
 
-        # self.exponents = exponents
-        self._validate_exponents(exponents)
-        self._validate_order(order)
         self.normalize = normalize
 
     def __repr__(self):
@@ -122,35 +140,62 @@ class LaurentPolynomial(InputValidation):
 
             for coefficient, exponent in zip(self.coefficients, self.exponents):
 
+                # If the coefficient is zero, then don't show the term
+
+                # First, determine whether a leading + or - sign is needed
+
+                # If this is the first term, then don't show the addition sign (e.g., +1+x^2 --> 1+x^2)
+                # Note: It is okay to display a leading minus sign
+                if self.coefficients.index(coefficient) == 0 and coefficient >= 0:
+                    pass
+                elif coefficient > 0:
+                    polynomial += "+"
+                elif coefficient == 0:
+                    pass
+                elif coefficient < 0:
+                    polynomial += "-"
+
+                # Next, determine how to show the coefficient
+
                 # If the coefficient is zero, then the whole term is zero --> don't show it
                 if coefficient == 0:
                     pass
 
+                # Don't show a coefficient of one if the exponent is nonzero (e.g., 1x^2 --> x^2)
+                elif coefficient == 1 and exponent != 0:
+                    pass
+
+                # Otherwise show the coefficient (strip minus signs since this is already accounted for)
                 else:
+                    polynomial += str(abs(coefficient))
 
-                    # First, determine whether a leading + or - sign is needed
+                # Next, determine how to show the term
 
-                    # If this is the first term, then don't show the addition sign (e.g., +1+x^2 --> 1+x^2)
-                    # Note: It is okay to display a leading minus sign
-                    if self.coefficients.index(coefficient) == 0 and coefficient >= 0:
-                        pass
-                    elif coefficient >= 0:
-                        polynomial += "+"
-                    elif coefficient < 0:
-                        polynomial += "-"
+                # If the exponent is zero, then don't show the term (e.g., 3x^0 --> 3)
+                if exponent == 0:
+                    pass
 
-                    # Next, determine how to show the coefficient and exponent
+                else:
+                    polynomial += self.term
 
-                    # Don't show a coefficient of one if the exponent is nonzero (e.g., 1x^1 --> x)
-                    if coefficient == 1 and exponent == 1:
-                        polynomial += str(self.term)
+                # Finally, determine how to show the exponent
 
-                    # But if a coefficient is nonzero and the exponent is zero, show it (e.g., 3x^0 --> 3)
-                    elif coefficient != 0 and exponent == 0:
-                        polynomial += str(coefficient)
+                # Don't show an exponent of zero or one (e.g., A^0 --> A, A^1 --> A)
+                if exponent == 0 or exponent == 1:
+                    pass
+                else:
+                    polynomial += "^" + str(exponent)
 
-                    else:
-                        polynomial += str(coefficient) + str(self.term) + "^" + str(exponent)
+
+                # Also don't show an exponent if its one (e.g., 3x^1 --> 3x)
+
+
+                # # But if a coefficient is nonzero and the exponent is zero, show it (e.g., 3x^0 --> 3)
+                # elif coefficient != 0 and exponent == 0:
+                #     polynomial += str(coefficient)
+                #
+                # else:
+                #     polynomial += str(coefficient) + str(self.term) + "^" + str(exponent)
 
 
             return polynomial
@@ -197,10 +242,11 @@ class LaurentPolynomial(InputValidation):
 
         TODO check if need to expand...
         TODO fix object creation statement
+        TODO Fix 0*self should be 0 (and simplify like terms before returning new object)
         
         """
         
-        # Ensure the input being multipled (the multiple) is a LaurentPolynomial or throw an error
+        # Ensure the input being multiple (the multiple) is a LaurentPolynomial or throw an error
         multiple = self._format_polynomial(multiple)
 
         product_coeffs = []
@@ -215,10 +261,7 @@ class LaurentPolynomial(InputValidation):
                 product_coeffs.append(multiple_coeff * self_coeff)
                 product_orders.append(multiple_order + self_order)
 
-        # Simplify like terms
-        product_coeffs, product_orders = self._simplify_expression(product_coeffs, product_orders)
-
-        return LaurentPolynomial(self.term, coefficients= product_coeffs, exponents= product_orders)
+        return LaurentPolynomial(self.term, coefficients=product_coeffs, exponents=product_orders)
 
 
     def __imul__(self, multiple):
@@ -241,41 +284,30 @@ class LaurentPolynomial(InputValidation):
     # TODO Implement __floordiv__ method?
     # TODO Implement _normalize method?
     # TODO Implement _sort method?
-    
-    def _format_polynomial(self, input):
-        """_check_input_format Converts input to LaurentPolynomial for arithmetic operations
 
-        :param input: _description_
-        :type input: _type_
-        :raises TypeError: _description_
-        :return: _description_
-        :rtype: _type_
-        """
-        if isinstance(input, LaurentPolynomial):
-            return input
-
-        elif isinstance(input, int) or isinstance(input, float):
-            coeff = [input]
-            order = [0]
-            return LaurentPolynomial(self.term, coefficients=coeff, exponents=order)
-
-        else:
-            raise TypeError("Polynomial must be of type LaurentPolynomial, int, or float")
-
-    def _simplify_expression(self, coeffs, orders):
+    def _simplify_expression(self):
         
-        new_coeffs = []
-        new_orders = []
+        new_coefficients = []
+        new_exponents = []
 
-        for coeff, order in zip(coeffs, orders):
+        for coefficient, exponent in zip(self.coefficients, self.exponents):
                 
-                if order in new_orders:
-                    new_coeffs[new_orders.index(order)] += coeff
-                else:
-                    new_coeffs.append(coeff)
-                    new_orders.append(order)
+            if coefficient != 0 and exponent not in new_exponents:
+                new_coefficients.append(coefficient)
+                new_exponents.append(exponent)
 
-        return new_coeffs, new_orders
+            elif coefficient != 0 and exponent in new_exponents:
+                new_coefficients[new_exponents.index(exponent)] += coefficient
+
+            else:
+                pass
+
+            # If the lists are completely empty, add a zero term
+            if len(new_coefficients) == 0 and len(new_exponents) == 0:
+                new_coefficients.append(0)
+                new_exponents.append(0)
+
+        self.coefficients, self.exponents = new_coefficients, new_exponents
 
     def _sort(self):
         pass
