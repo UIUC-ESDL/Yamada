0407f58bdfbf45e004793625866211a32371cfc5
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-27 13:36:20 -0600

    Various updates

--- docs/source/examples.rst
+++ docs/source/examples.rst
@@ -8,3 +8,10 @@ My Title
    Example 1 <https://github.com/SPI2Py/Yamada/blob/main/examples/calculate_yamada_polynomial_manual/demo.ipynb>
    Example 2 <https://github.com/SPI2Py/Yamada/blob/main/examples/calculate_yamada_polynomial_semiautomatic/demo.ipynb>
    Example 3 <https://github.com/SPI2Py/Yamada/blob/main/examples/calculate_yamada_polynomial_automatic/demo.ipynb>
+
+
+How to calculate Yamada polynomials
+
+.. image:: /images/publication/class_of_spins_for_a_crossing.png
+    :width: 500
+    :alt: Class of spins for a crossing
--- src/yamada/projection.py
+++ src/yamada/projection.py
@@ -52,7 +52,15 @@ d3 = np.array([[0, 0, 1], [0, 0.1, 0]])
 
 
 # fig = plt.figure()
-fig, (ax1, ax2) = plt.subplots()
+
+fig = plt.figure(figsize=plt.figaspect(0.5))
+
+ax1 = fig.add_subplot(221, projection='3d')
+ax2 = fig.add_subplot(222, projection='3d')
+ax3 = fig.add_subplot(223)
+ax4 = fig.add_subplot(224)
+
+# fig, (ax1, ax2) = plt.subplots()
 
 ax1.set_xlim(-1, 1)
 ax1.set_ylim(-1, 1)
@@ -68,103 +76,152 @@ ax1.plot3D(b3[:, 0], b3[:, 1], b3[:, 2], 'blue')
 ax1.plot3D(c3[:, 0], c3[:, 1], c3[:, 2], 'blue')
 ax1.plot3D(d3[:, 0], d3[:, 1], d3[:, 2], 'blue')
 
-# plt.show()
+
+# Plot the 2D projection
+
+ax3.title.set_text('2D Projection into the XZ plane')
+ax3.xaxis.label.set_text('x')
+ax3.yaxis.label.set_text('z')
+
+ax3.set_xlim(-1, 1)
+ax3.set_ylim(-1, 1)
+
+a2 = LineString([(a3[0][0], a3[0][2]), (a3[1][0], a3[1][2])])
+b2 = LineString([(b3[0][0], b3[0][2]), (b3[1][0], b3[1][2])])
+c2 = LineString([(c3[0][0], c3[0][2]), (c3[1][0], c3[1][2])])
+d2 = LineString([(d3[0][0], d3[0][2]), (d3[1][0], d3[1][2])])
+
+print(a2.intersects(c2))
+
+i1 = a2.intersection(c2)
+
+
+
+plot_line(a2, ax=ax3)
+plot_line(b2, ax=ax3)
+plot_line(c2, ax=ax3)
+plot_line(d2, ax=ax3)
+
+plot_points(i1, ax=ax3,
+            marker='o',
+            markersize=40,
+            markerfacecolor='none',
+            markeredgecolor='red',
+            markeredgewidth=4)
+
+
+
+
+
+
 
 # Apply a random 3D transformation
 
-# # fig = plt.figure()
+# fig = plt.figure()
 # ax = plt.axes(projection='3d')
-#
-# ax.set_xlim(-1, 1)
-# ax.set_ylim(-1, 1)
-# ax.set_zlim(-1, 1)
-#
-# ax.title.set_text('3D points')
-# ax.xaxis.label.set_text('x')
-# ax.yaxis.label.set_text('y')
-# ax.zaxis.label.set_text('z')
-#
-# positions = np.concatenate((a3, b3, c3, d3), axis=0)
-#
-# angles = np.random.rand(3) * 2 * np.pi
-#
-# new_positions = rotate_about_point(positions, angles)
-#
-# a3 = new_positions[0:2]
-# b3 = new_positions[2:4]
-# c3 = new_positions[4:6]
-# d3 = new_positions[6:8]
-#
-# ax.plot3D(a3[:, 0], a3[:, 1], a3[:, 2], 'blue')
-# ax.plot3D(b3[:, 0], b3[:, 1], b3[:, 2], 'blue')
-# ax.plot3D(c3[:, 0], c3[:, 1], c3[:, 2], 'blue')
-# ax.plot3D(d3[:, 0], d3[:, 1], d3[:, 2], 'blue')
-#
 
 
 
-plt.show()
+ax2.set_xlim(-1, 1)
+ax2.set_ylim(-1, 1)
+ax2.set_zlim(-1, 1)
 
-# # Project 3D points to 2D on the XZ plane
-#
+ax2.title.set_text('3D points')
+ax2.xaxis.label.set_text('x')
+ax2.yaxis.label.set_text('y')
+ax2.zaxis.label.set_text('z')
+
+positions = np.concatenate((a3, b3, c3, d3), axis=0)
+
+angles = np.random.rand(3,seed=1) * 2 * np.pi
+
+new_positions = rotate_about_point(positions, angles)
+
+a3 = new_positions[0:2]
+b3 = new_positions[2:4]
+c3 = new_positions[4:6]
+d3 = new_positions[6:8]
+
+ax2.plot3D(a3[:, 0], a3[:, 1], a3[:, 2], 'blue')
+ax2.plot3D(b3[:, 0], b3[:, 1], b3[:, 2], 'blue')
+ax2.plot3D(c3[:, 0], c3[:, 1], c3[:, 2], 'blue')
+ax2.plot3D(d3[:, 0], d3[:, 1], d3[:, 2], 'blue')
+
+
+####
+
+
+
+
+# plt.show()
+
+# Project 3D points to 2D on the XZ plane
+
 # fig = plt.figure()
 # ax = fig.add_subplot()
-#
-# a2 = LineString([(a3[0][0], a3[0][2]), (a3[1][0], a3[1][2])])
-# b2 = LineString([(b3[0][0], b3[0][2]), (b3[1][0], b3[1][2])])
-# c2 = LineString([(c3[0][0], c3[0][2]), (c3[1][0], c3[1][2])])
-# d2 = LineString([(d3[0][0], d3[0][2]), (d3[1][0], d3[1][2])])
-#
-# def project_3d_to_2d(points_3d):
-#     pass
-#
-#
-# def check_for_overlapping_points(*line_strings):
-#     # TODO Check function logic
-#     for i in range(len(line_strings)):
-#         for j in range(i + 1, len(line_strings)):
-#             if line_strings[i].intersects(line_strings[j]):
-#                 return True
-#     return False
-#
-#
-# def check_for_parallel_lines(*line_strings):
-#     # TODO Check function logic
-#     for i in range(len(line_strings)):
-#         for j in range(i + 1, len(line_strings)):
-#             if line_strings[i].parallel(line_strings[j]):
-#                 return True
-#     return False
-#
-#
-# def check_for_intersecting_lines(*line_strings):
-#     # TODO Check function logic
-#     intersections = []
-#     for i in range(len(line_strings)):
-#         for j in range(i + 1, len(line_strings)):
-#             if line_strings[i].intersects(line_strings[j]):
-#                 intersections.append(line_strings[i].intersection(line_strings[j]))
-#     return intersections
-#
-#
-#
-#
-# print(a2.intersects(c2))
-#
-# i1 = a2.intersection(c2)
-#
-#
-#
-# plot_line(a2, ax=ax)
-# plot_line(b2, ax=ax)
-# plot_line(c2, ax=ax)
-# plot_line(d2, ax=ax)
-#
-# plot_points(i1, ax=ax,
-#             marker='o',
-#             markersize=40,
-#             markerfacecolor='none',
-#             markeredgecolor='red',
-#             markeredgewidth=4)
-#
-# plt.show()
+
+
+
+def project_3d_to_2d(points_3d):
+    pass
+
+
+def check_for_overlapping_points(*line_strings):
+    # TODO Check function logic
+    for i in range(len(line_strings)):
+        for j in range(i + 1, len(line_strings)):
+            if line_strings[i].intersects(line_strings[j]):
+                return True
+    return False
+
+
+def check_for_parallel_lines(*line_strings):
+    # TODO Check function logic
+    for i in range(len(line_strings)):
+        for j in range(i + 1, len(line_strings)):
+            if line_strings[i].parallel(line_strings[j]):
+                return True
+    return False
+
+
+def check_for_intersecting_lines(*line_strings):
+    # TODO Check function logic
+    intersections = []
+    for i in range(len(line_strings)):
+        for j in range(i + 1, len(line_strings)):
+            if line_strings[i].intersects(line_strings[j]):
+                intersections.append(line_strings[i].intersection(line_strings[j]))
+    return intersections
+
+
+ax3.title.set_text('2D Projection into the XZ plane')
+ax3.xaxis.label.set_text('x')
+ax3.yaxis.label.set_text('z')
+
+ax3.set_xlim(-1, 1)
+ax3.set_ylim(-1, 1)
+
+a2 = LineString([(a3[0][0], a3[0][2]), (a3[1][0], a3[1][2])])
+b2 = LineString([(b3[0][0], b3[0][2]), (b3[1][0], b3[1][2])])
+c2 = LineString([(c3[0][0], c3[0][2]), (c3[1][0], c3[1][2])])
+d2 = LineString([(d3[0][0], d3[0][2]), (d3[1][0], d3[1][2])])
+
+print(a2.intersects(c2))
+
+i1 = a2.intersection(c2)
+
+
+
+plot_line(a2, ax=ax4)
+plot_line(b2, ax=ax4)
+plot_line(c2, ax=ax4)
+plot_line(d2, ax=ax4)
+
+plot_points(i1, ax=ax4,
+            marker='o',
+            markersize=40,
+            markerfacecolor='none',
+            markeredgecolor='red',
+            markeredgewidth=4)
+
+plt.show()
