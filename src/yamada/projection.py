fc1b15d17c864f9d76ed5d874d5c729d724199fe
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-02-24 10:25:52 -0600

    Various tweaks

--- src/yamada/projection.py
+++ src/yamada/projection.py
@@ -8,45 +8,6 @@ from mpl_toolkits.mplot3d import Axes3D
 from shapely.geometry import LineString
 from shapely.plotting import plot_line, plot_points
 
-def rotate_about_point(positions, rotation):
-    """
-    Rotates a set of points about the first 3D point in the array.
-
-    :param positions:
-    :param rotation: Angle in radians
-    :return: new_positions:
-    """
-
-    # Shift the object to origin
-    reference_position = positions[0]
-    origin_positions = positions - reference_position
-
-    alpha, beta, gamma = rotation
-
-    # Rotation matrix Euler angle convention r = r_z(gamma) @ r_y(beta) @ r_x(alpha)
-
-    r_x = np.array([[1., 0., 0.],
-                    [0., cos(alpha), -sin(alpha)],
-                    [0., sin(alpha), cos(alpha)]])
-
-    r_y = np.array([[cos(beta), 0., sin(beta)],
-                    [0., 1., 0.],
-                    [-sin(beta), 0., cos(beta)]])
-
-    r_z = np.array([[cos(gamma), -sin(gamma), 0.],
-                    [sin(gamma), cos(gamma), 0.],
-                    [0., 0., 1.]])
-
-    r = r_z @ r_y @ r_x
-
-    # Transpose positions from [[x1,y1,z1],[x2... ] to [[x1,x2,x3],[y1,... ]
-    rotated_origin_positions = (r @ origin_positions.T).T
-
-    # Shift back from origin
-    new_positions = rotated_origin_positions + reference_position
-
-    return new_positions
-
 
 def get_line_intersection(a, b, c, d):
     """
@@ -173,11 +134,53 @@ class SpatialTopology:
         self.node_positions = node_positions
         self.edges = edges
 
+        # Positions for the 2D projections / rotation
+
+    @property
+    def random_rotation(self):
+        return np.random.rand(3) * 2 * np.pi
+
     def project(self):
         pass
 
     def rotate(self):
-        pass
+        """
+        Rotates a set of points about the first 3D point in the array.
+
+        :param positions:
+        :param rotation: Angle in radians
+        :return: new_positions:
+        """
+
+        # Shift the object to origin
+        reference_position = self.node_positions[0]
+        origin_positions = positions - reference_position
+
+        alpha, beta, gamma = rotation
+
+        # Rotation matrix Euler angle convention r = r_z(gamma) @ r_y(beta) @ r_x(alpha)
+
+        r_x = np.array([[1., 0., 0.],
+                        [0., cos(alpha), -sin(alpha)],
+                        [0., sin(alpha), cos(alpha)]])
+
+        r_y = np.array([[cos(beta), 0., sin(beta)],
+                        [0., 1., 0.],
+                        [-sin(beta), 0., cos(beta)]])
+
+        r_z = np.array([[cos(gamma), -sin(gamma), 0.],
+                        [sin(gamma), cos(gamma), 0.],
+                        [0., 0., 1.]])
+
+        r = r_z @ r_y @ r_x
+
+        # Transpose positions from [[x1,y1,z1],[x2... ] to [[x1,x2,x3],[y1,... ]
+        rotated_origin_positions = (r @ origin_positions.T).T
+
+        # Shift back from origin
+        new_positions = rotated_origin_positions + reference_position
+
+        return new_positions
 
     def plot(self):
         for edge in self.edges:
@@ -194,11 +197,11 @@ sp1.plot()
 
 # Plot 3D points
 
-
-a3 = np.array([[0, 0.5, 0], [1, 0.5, 1]])
-b3 = np.array([[1, 0.5, 1], [1, 0, 0]])
-c3 = np.array([[1, 0, 0], [0, 0, 1]])
-d3 = np.array([[0, 0, 1], [0, 0.5, 0]])
+#
+# a3 = np.array([[0, 0.5, 0], [1, 0.5, 1]])
+# b3 = np.array([[1, 0.5, 1], [1, 0, 0]])
+# c3 = np.array([[1, 0, 0], [0, 0, 1]])
+# d3 = np.array([[0, 0, 1], [0, 0.5, 0]])
 
 # ax1.plot3D(a3[:, 0], a3[:, 1], a3[:, 2], 'blue')
 # ax1.plot3D(b3[:, 0], b3[:, 1], b3[:, 2], 'blue')
@@ -208,70 +211,70 @@ d3 = np.array([[0, 0, 1], [0, 0.5, 0]])
 
 # Plot the 2D projection
 
-a2 = LineString([(a3[0][0], a3[0][2]), (a3[1][0], a3[1][2])])
-b2 = LineString([(b3[0][0], b3[0][2]), (b3[1][0], b3[1][2])])
-c2 = LineString([(c3[0][0], c3[0][2]), (c3[1][0], c3[1][2])])
-d2 = LineString([(d3[0][0], d3[0][2]), (d3[1][0], d3[1][2])])
-
-i1 = a2.intersection(c2)
-
-ax3.plot([a3[0][0], a3[1][0]], [a3[0][2], a3[1][2]], 'blue')
-ax3.plot([b3[0][0], b3[1][0]], [b3[0][2], b3[1][2]], 'blue')
-ax3.plot([c3[0][0], c3[1][0]], [c3[0][2], c3[1][2]], 'blue')
-ax3.plot([d3[0][0], d3[1][0]], [d3[0][2], d3[1][2]], 'blue')
-
-
-# plot_line(a2, ax=ax3)
-# plot_line(b2, ax=ax3)
-# plot_line(c2, ax=ax3)
-# plot_line(d2, ax=ax3)
-
-plot_points(i1, ax=ax3,
-            marker='o',
-            markersize=40,
-            markerfacecolor='none',
-            markeredgecolor='red',
-            markeredgewidth=4)
-
-
-
-# Apply a random 3D transformation
-
-positions = np.concatenate((a3, b3, c3, d3), axis=0)
-
-np.random.seed(1)
-angles = np.random.rand(3) * 2 * np.pi
-
-new_positions = rotate_about_point(positions, angles)
-
-a3 = new_positions[0:2]
-b3 = new_positions[2:4]
-c3 = new_positions[4:6]
-d3 = new_positions[6:8]
-
-ax2.plot3D(a3[:, 0], a3[:, 1], a3[:, 2], 'blue')
-ax2.plot3D(b3[:, 0], b3[:, 1], b3[:, 2], 'blue')
-ax2.plot3D(c3[:, 0], c3[:, 1], c3[:, 2], 'blue')
-ax2.plot3D(d3[:, 0], d3[:, 1], d3[:, 2], 'blue')
-
-a2 = LineString([(a3[0][0], a3[0][2]), (a3[1][0], a3[1][2])])
-b2 = LineString([(b3[0][0], b3[0][2]), (b3[1][0], b3[1][2])])
-c2 = LineString([(c3[0][0], c3[0][2]), (c3[1][0], c3[1][2])])
-d2 = LineString([(d3[0][0], d3[0][2]), (d3[1][0], d3[1][2])])
-
-i1 = a2.intersection(c2)
-
-plot_line(a2, ax=ax4)
-plot_line(b2, ax=ax4)
-plot_line(c2, ax=ax4)
-plot_line(d2, ax=ax4)
-
-plot_points(i1, ax=ax4,
-            marker='o',
-            markersize=40,
-            markerfacecolor='none',
-            markeredgecolor='red',
-            markeredgewidth=4)
+# a2 = LineString([(a3[0][0], a3[0][2]), (a3[1][0], a3[1][2])])
+# b2 = LineString([(b3[0][0], b3[0][2]), (b3[1][0], b3[1][2])])
+# c2 = LineString([(c3[0][0], c3[0][2]), (c3[1][0], c3[1][2])])
+# d2 = LineString([(d3[0][0], d3[0][2]), (d3[1][0], d3[1][2])])
+#
+# i1 = a2.intersection(c2)
+#
+# ax3.plot([a3[0][0], a3[1][0]], [a3[0][2], a3[1][2]], 'blue')
+# ax3.plot([b3[0][0], b3[1][0]], [b3[0][2], b3[1][2]], 'blue')
+# ax3.plot([c3[0][0], c3[1][0]], [c3[0][2], c3[1][2]], 'blue')
+# ax3.plot([d3[0][0], d3[1][0]], [d3[0][2], d3[1][2]], 'blue')
+#
+#
+# # plot_line(a2, ax=ax3)
+# # plot_line(b2, ax=ax3)
+# # plot_line(c2, ax=ax3)
+# # plot_line(d2, ax=ax3)
+#
+# plot_points(i1, ax=ax3,
+#             marker='o',
+#             markersize=40,
+#             markerfacecolor='none',
+#             markeredgecolor='red',
+#             markeredgewidth=4)
+#
+#
+#
+# # Apply a random 3D transformation
+#
+# positions = np.concatenate((a3, b3, c3, d3), axis=0)
+#
+# np.random.seed(1)
+# angles = np.random.rand(3) * 2 * np.pi
+#
+# new_positions = rotate_about_point(positions, angles)
+#
+# a3 = new_positions[0:2]
+# b3 = new_positions[2:4]
+# c3 = new_positions[4:6]
+# d3 = new_positions[6:8]
+#
+# ax2.plot3D(a3[:, 0], a3[:, 1], a3[:, 2], 'blue')
+# ax2.plot3D(b3[:, 0], b3[:, 1], b3[:, 2], 'blue')
+# ax2.plot3D(c3[:, 0], c3[:, 1], c3[:, 2], 'blue')
+# ax2.plot3D(d3[:, 0], d3[:, 1], d3[:, 2], 'blue')
+#
+# a2 = LineString([(a3[0][0], a3[0][2]), (a3[1][0], a3[1][2])])
+# b2 = LineString([(b3[0][0], b3[0][2]), (b3[1][0], b3[1][2])])
+# c2 = LineString([(c3[0][0], c3[0][2]), (c3[1][0], c3[1][2])])
+# d2 = LineString([(d3[0][0], d3[0][2]), (d3[1][0], d3[1][2])])
+#
+# i1 = a2.intersection(c2)
+#
+# plot_line(a2, ax=ax4)
+# plot_line(b2, ax=ax4)
+# plot_line(c2, ax=ax4)
+# plot_line(d2, ax=ax4)
+#
+# plot_points(i1, ax=ax4,
+#             marker='o',
+#             markersize=40,
+#             markerfacecolor='none',
+#             markeredgecolor='red',
+#             markeredgewidth=4)
 
 
 
