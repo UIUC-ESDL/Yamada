829a070930bdf5e2b2882e6566373b05fe5ce8f1
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-02-27 22:26:11 -0600

    Move some code to the dedicated examples files

--- examples/calculate_yamada_polynomials/automatic_demo.py
+++ examples/calculate_yamada_polynomials/automatic_demo.py
@@ -1,15 +1,24 @@
 import numpy as np
 from yamada.projection import SpatialGraph
 
-# Seed 0 produces no crossings
-# Seed 1 produces 2 crossings
+np.random.seed(0)
 
-# np.random.seed(1)
+# sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd'],
+#                    node_positions=np.array([[0, 0.5, 0], [1, 0.5, 1], [1, 0, 0], [0, 0, 1]]),
+#                    edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')])
+# sg1.project()
+# sg1.plot()
+# sgd1 = sg1.create_spatial_graph_diagram()
+# yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
+# print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
+
+
 
 sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd', 'e', 'f'],
                    node_positions=np.array([[0, 0.5, 0], [1, 0, 1], [2, 0.5, 0], [3, 0, 1], [1, 1, 0], [-1, 0, 1]]),
                    edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e'), ('e', 'f'), ('f', 'a')])
 
+# TODO Question: Why does adding edge ('a', 'e') cause the graph to be non-planar?
 
 sg1.project()
 
@@ -24,18 +33,3 @@ print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
 
 
 
-# np.random.seed(0)
-#
-# sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd'],
-#                    node_positions=np.array([[0, 0.5, 0], [1, 0.5, 1], [1, 0, 0], [0, 0, 1]]),
-#                    edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')])
-#
-# sg1.project()
-#
-# sg1.plot()
-#
-# sgd1 = sg1.create_spatial_graph_diagram()
-#
-# yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
-#
-# print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
