3e460c009f4d341a333911bb0a7cc25adf6ffd14
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-03-02 13:11:05 -0600

    Yup it's working for the examples I generate!

--- examples/calculate_yamada_polynomials/automatic_demo.py
+++ examples/calculate_yamada_polynomials/automatic_demo.py
@@ -1,49 +1,50 @@
 import numpy as np
 from yamada.projection import SpatialGraph
 
-np.random.seed(1)
+for i in range(6):
+    np.random.seed(i)
 
-# Random seed 0 works
-# Random seed 1 does not work
+    # Random seed 0 works
+    # Random seed 1 does not work
 
-# sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd'],
-#                    node_positions=np.array([[0, 0.5, 0], [1, 0.5, 1], [1, 0, 0], [0, 0, 1]]),
-#                    edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')])
-# sg1.project()
-# sg1.plot()
-# sgd1 = sg1.create_spatial_graph_diagram()
-# yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
-# print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
-#
-# node_list = sg1.get_adjacent_nodes('a')
-#
-# node_ordering = sg1.cyclic_edge_order_vertex('a')
-#
-# crossing_ordering = sg1.cyclic_edge_order_crossing(0)
+    # sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd'],
+    #                    node_positions=np.array([[0, 0.5, 0], [1, 0.5, 1], [1, 0, 0], [0, 0, 1]]),
+    #                    edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')])
+    # sg1.project()
+    # sg1.plot()
+    # sgd1 = sg1.create_spatial_graph_diagram()
+    # yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
+    # print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
+    #
+    # node_list = sg1.get_adjacent_nodes('a')
+    #
+    # node_ordering = sg1.cyclic_edge_order_vertex('a')
+    #
+    # crossing_ordering = sg1.cyclic_edge_order_crossing(0)
 
-# node_pairs = sg1.get_adjacent_edges_without_crossings('a')
+    # node_pairs = sg1.get_adjacent_edges_without_crossings('a')
 
-# sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd', 'e', 'f'],
-#                    node_positions=np.array([[0, 0.5, 0], [1, 0, 1], [2, 0.5, 0], [3, 0, 1], [1, 1, 0], [-1, 0, 1]]),
-#                    edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e'), ('e', 'f'), ('f', 'a')])
-#
-# sg1.project()
-# sg1.plot()
-# sgd1 = sg1.create_spatial_graph_diagram()
-# yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
-# print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
-
-
-sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd', 'e', 'f'],
-                   node_positions=np.array([[0, 0.5, 0], [1, 0, 1], [2, 0.5, 0], [3, 0, 1], [1, 1, 0], [-1, 0, 1]]),
-                   edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e'), ('e', 'f'), ('f', 'a'), ('b', 'f')])
-
-sg1.project()
-sg1.plot()
+    # sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd', 'e', 'f'],
+    #                    node_positions=np.array([[0, 0.5, 0], [1, 0, 1], [2, 0.5, 0], [3, 0, 1], [1, 1, 0], [-1, 0, 1]]),
+    #                    edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e'), ('e', 'f'), ('f', 'a')])
+    #
+    # sg1.project()
+    # sg1.plot()
+    # sgd1 = sg1.create_spatial_graph_diagram()
+    # yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
+    # print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
 
 
-sgd1 = sg1.create_spatial_graph_diagram()
+    sg1 = SpatialGraph(nodes=['a', 'b', 'c', 'd', 'e', 'f'],
+                       node_positions=np.array([[0, 0.5, 0], [1, 0, 1], [2, 0.5, 0], [3, 0, 1], [1, 1, 0], [-1, 0, 1]]),
+                       edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e'), ('e', 'f'), ('f', 'a'), ('b', 'f')])
 
-yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
-#
-print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
+    sg1.project()
+    sg1.plot()
+
+
+    sgd1 = sg1.create_spatial_graph_diagram()
+
+    yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
+    #
+    print("Infinity Symbol Yamada Polynomial:", yamada_polynomial_infinity_symbol)
