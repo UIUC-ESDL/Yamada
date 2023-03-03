5c259e08778a46615bd8400ff7f6d2884b28f8ab
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-03-02 22:17:45 -0600

    Add unit tests

--- tests/test_yamada/test_yamada_polynomial_calculation_automatically_labeled.py
+++ tests/test_yamada/test_yamada_polynomial_calculation_automatically_labeled.py
@@ -3,8 +3,16 @@ from yamada.projection import SpatialGraph
 
 
 def test_unknot():
-    # TODO Implement this test.
-    pass
+    for i in range(6):
+        np.random.seed(i)
+
+        sg1 = SpatialGraph(nodes=['a', 'b', 'c'],
+                           node_positions=np.array([[0, 0.5, 0], [-1, 0.5, 1], [1, 0, 0]]),
+                           edges=[('a', 'b'), ('b', 'c'), ('c', 'a')])
+        sg1.project()
+        sgd1 = sg1.create_spatial_graph_diagram()
+        sgd1.yamada_polynomial()
+
 
 
 def test_infinity_symbol_single_twist():
@@ -15,7 +23,7 @@ def test_infinity_symbol_single_twist():
                        edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')])
     sg1.project()
     sgd1 = sg1.create_spatial_graph_diagram()
-    yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
+    sgd1.yamada_polynomial()
 
 def test_infinity_symbol_double_twist():
     for i in range(6):
@@ -25,7 +33,7 @@ def test_infinity_symbol_double_twist():
                            edges=[('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e'), ('e', 'f'), ('f', 'a')])
         sg1.project()
         sgd1 = sg1.create_spatial_graph_diagram()
-        yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
+        sgd1.yamada_polynomial()
 
 def test_double_crossing_single_edge():
     # TODO Implement this test.
@@ -48,4 +56,4 @@ def test_complicated_topology_1():
 
         sg1.project()
         sgd1 = sg1.create_spatial_graph_diagram()
-        yamada_polynomial_infinity_symbol = sgd1.yamada_polynomial()
+        sgd1.yamada_polynomial()
