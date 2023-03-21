bcce1e0bd79e043231f9c392ba3eda9d89cbcc6f
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-03-21 13:38:38 -0500

    Refactor code

--- src/yamada/__init__.py
+++ src/yamada/__init__.py
@@ -1,4 +1,4 @@
-from yamada.h_poly import h_poly, has_cut_edge, remove_valence_two_vertices
+from yamada.H_polynomial import h_poly, has_cut_edge, remove_valence_two_vertices
 from yamada.spatial_graph_diagrams import SpatialGraphDiagram, Vertex, Edge, \
     Crossing, reverse_poly, normalize_yamada_polynomial
-from yamada.projection import SpatialGraph, LinearAlgebra
+from yamada.spatial_graphs import SpatialGraph, LinearAlgebra
