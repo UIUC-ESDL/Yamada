498815870f2cfccd11fb9836e962b862004f3d5d
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-07-15 12:35:25 -0500

    Add troubleshooting script

--- examples/workflow_troubleshooting.py
+++ examples/workflow_troubleshooting.py
@@ -2,6 +2,7 @@ import numpy as np
 import networkx as nx
 import json
 import matplotlib
+import time
 import matplotlib.pyplot as plt
 from yamada import SpatialGraph
 
@@ -13,53 +14,64 @@ print('Starting')
 input_file = 'output_data.json'
 
 with open(input_file, 'r') as f:
-    graphs = json.load(f)
-
-graph1 = list(graphs.values())[0]
+    graph_data = json.load(f)
 
 
-edges, nodes, node_positions = graph1
+graphs = list(graph_data.values())
 
-indices = []
 sgs = []
 sgds = []
 yps = []
+num_graphs = 20
 
-# for i, (edges, nodes, node_positions) in data.items():
+print('Time', time.time())
 
-# Reformat edges
-edges = [(a, b) for a, b in edges]
+# TODO Add logic for num of crossings
 
-# Reformat positions as a dict
-pos = {node: np.array(position) for node, position in zip(nodes, node_positions)}
+for i, graph in enumerate(graphs):
 
-g = nx.Graph()
-g.add_nodes_from(nodes)
-g.add_edges_from(edges)
+    print('Loop', i, 'time', time.time())
 
-# Plot the networkx graph in 3d
-node_xyz = np.array([pos[v] for v in g])
-# node_xyz = np.array([pos[v] for v in sorted(g)])
-edge_xyz = np.array([(pos[u], pos[v]) for u, v in g.edges()])
+    edges, nodes, node_positions = graph
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
+    # Reformat edges
+    edges = [(a, b) for a, b in edges]
 
-ax.scatter(*node_xyz.T, s=100, ec="w")
-ax.scatter(*node_xyz[[1,2]].T, s=200, c="red")
-ax.scatter(*node_xyz[[91,39]].T, s=200, c="green")
+    # Reformat positions as a dict
+    pos = {node: np.array(position) for node, position in zip(nodes, node_positions)}
 
-for vizedge in edge_xyz:
-    ax.plot(*vizedge.T, color="tab:gray")
+    g = nx.Graph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
 
-plt.show()
+    # Plot the networkx graph in 3d
+    node_xyz = np.array([pos[v] for v in g])
+    # edge_xyz = np.array([(pos[u], pos[v]) for u, v in g.edges()])
+    #
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection="3d")
+    #
+    # ax.scatter(*node_xyz.T, s=100, ec="w")
+    # ax.scatter(*node_xyz[[1,2]].T, s=200, c="red")
+    # ax.scatter(*node_xyz[[91,39]].T, s=200, c="green")
+    #
+    # for vizedge in edge_xyz:
+    #     ax.plot(*vizedge.T, color="tab:gray")
+    #
+    # plt.show()
 
-sg = SpatialGraph(nodes=nodes, edges=edges, node_positions=node_xyz)
-sg.plot()
+    sg = SpatialGraph(nodes=nodes, edges=edges, node_positions=node_xyz)
+    sg.plot()
 
-sgd = sg.create_spatial_graph_diagram()
-yp = sgd.normalized_yamada_polynomial()
+    sgd = sg.create_spatial_graph_diagram()
+    yp = sgd.normalized_yamada_polynomial()
 
+    sgs.append(sg)
+    sgds.append(sgd)
+    yps.append(yp)
+
+    if i >= num_graphs-1:
+        break
 
 
 
